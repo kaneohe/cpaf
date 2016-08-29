96678defdce93bb4aa85f3043735a6041e39fd9d
Jeremy Moseley <jeremymoeley@me.com>
2016-08-29 12:54:52 -0700

    Specified stylesheet properly

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function enqueue_scripts() {
+	wp_deregister_style( 'main-stylesheet' );
+	wp_enqueue_style( 'cpaf', 
+		get_stylesheet_directory_uri() . '/style.css', 
+		array(), 
+		wp_get_theme()->get('Version'), 
+		'all' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
