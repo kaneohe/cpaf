117b278f60651717641a2c405c037d0d3b492a61
Brian Gunzenhauser <brian@boxandpixel.com>
2016-08-31 12:38:01 -0400

    Incremental sass update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function(){
 	return gulp.src('scss/style.scss')
 		.pipe(sass())
 		.pipe(gulp.dest(''))
 });
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('default',['watch']);
+
